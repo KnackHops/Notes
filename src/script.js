6b894ca147aa3d184e093592631196fe3d9475de
Aldrian <himalosc@gmail.com>
2021-03-18 16:34:40 +0800

    added more functionality for menu as well as styles

--- src/script.js
+++ src/script.js
@@ -16,26 +16,38 @@ const clicked = e => {
     let nodeClass = "." + (e.target.parentNode.className.replace("Btn",""));
 
     if(btnClass!=="closeBtn"){
-        e.type=="dblclick" ?  menuClicked(".noteMenu"):menuClicked(nodeClass);
+        e.type=="dblclick" ?  menuClicked(".noteMenu"):menuClicked(nodeClass,e);
     }else{
         closeBtnClicked(nodeClass);
     }
 }
 
-const menuClicked = nodeClass => {
+const menuClicked = (nodeClass, e=null) => {
+    if(e!==null){
+        if(e.target.className==="registerBtn"){
+            document.querySelector("section .emailContainer").classList.toggle("hiddenSection");
+        }
+    }
     document.querySelector(nodeClass).classList.toggle("hiddenSection");
 }
 
 const closeBtnClicked = nodeClass => {
+    if(nodeClass===".loginRegisterMenu"){
+        if(!document.querySelector("section .emailContainer").classList.contains("hiddenSection")){
+            document.querySelector("section .emailContainer").classList.toggle("hiddenSection");
+        }
+    }
     document.querySelector(nodeClass).classList.toggle("hiddenSection");
 }
 
 window.onload = ()=>{
     const sections = document.querySelectorAll("body section");
     const sectionBtns = document.querySelectorAll("section .closeBtn");
-    const loginMenuBtn = document.querySelector("ul .loginMenuBtn button");
-    const registerMenuBtn = document.querySelector("ul .registerMenuBtn button");
+    const loginMenuBtn = document.querySelector("ul .loginRegisterMenuBtn .loginBtn");
+    const registerMenuBtn = document.querySelector("ul .loginRegisterMenuBtn .registerBtn");
+    const registerInput = document.querySelector("section .emailContainer");
 
+    registerInput.classList.add("hiddenSection");
     sectionBtns.forEach(btn=>btn.addEventListener("click", clicked));
     sections.forEach(section=>section.classList.add("hiddenSection"));
 
